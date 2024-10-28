953a81ef94dbb450b8f4462a93c0e7fd1ca51d4d
Marcel Küpper <github@kuepper.net>
2024-10-28 14:04:33 +0100

    [!!!][TASK] Lets TYPO3 create needed database fields automatically
    Some minor deviations in the database field configuration might occur.
    In most cases this should not be a problem.
    
    This commit also contains a update wizard to migrate the related posts
    field in the pages table.
    
    Before this wizard would be executed, the database compare for this
    field will fail.

---
+++ Classes/Updates/RelatedPostsMigration.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Greenfieldr\Golb\Updates;
+
+use Doctrine\DBAL\Schema\Column;
+use TYPO3\CMS\Core\Database\Connection;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Database\Query\QueryBuilder;
+use TYPO3\CMS\Core\Registry;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Install\Attribute\UpgradeWizard;
+use TYPO3\CMS\Install\Updates\BackendGroupsExplicitAllowDenyMigration;
+use TYPO3\CMS\Install\Updates\DatabaseUpdatedPrerequisite;
+use TYPO3\CMS\Install\Updates\UpgradeWizardInterface;
+
+#[UpgradeWizard('relatedPostsMigration')]
+final class RelatedPostsMigration implements UpgradeWizardInterface
+{
+    protected const PAGES_TABLE = 'pages';
+    protected const RELATED_POSTS_FIELD = 'tx_golb_related';
+
+    public function __construct(
+        private readonly Registry $registry,
+        private readonly ConnectionPool $connectionPool,
+    ) {}
+
+    public function getTitle(): string
+    {
+        return 'Migrates pages field for related posts';
+    }
+
+    public function getDescription(): string
+    {
+        return 'The relational field in pages table had a wrong data type due to historical reasons. Lets migrate it.';
+    }
+
+    public function getPrerequisites(): array
+    {
+        return [];
+    }
+
+    public function updateNecessary(): bool
+    {
+        $queryBuilder = $this->connectionPool->getQueryBuilderForTable(self::PAGES_TABLE);
+        $queryBuilder->getRestrictions()->removeAll();
+
+        return ($queryBuilder->select(self::RELATED_POSTS_FIELD)->from(self::PAGES_TABLE)
+            ->where(
+                $queryBuilder->expr()->isNull(self::RELATED_POSTS_FIELD)
+            )->executeQuery()->rowCount() > 0);
+    }
+
+    public function executeUpdate(): bool
+    {
+        $queryBuilder = $this->connectionPool->getQueryBuilderForTable(self::PAGES_TABLE);
+        $queryBuilder->getRestrictions()->removeAll();
+
+        $queryBuilder->update(self::PAGES_TABLE)
+            ->set(self::RELATED_POSTS_FIELD, 0, true, Connection::PARAM_INT)
+            ->where($queryBuilder->expr()->isNull(self::RELATED_POSTS_FIELD))->executeStatement();
+
+        return true;
+    }
+}
